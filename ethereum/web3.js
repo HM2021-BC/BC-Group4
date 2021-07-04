2d93c8cb382b01619622dfbbd1f9dfba904504ac
Alexander <Alexander.Richter@adesso.de>
2021-07-04 17:18:32 +0200

    Add new structure

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -3,7 +3,7 @@ import window from 'global'
 
 let web3;
 // const network = "https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q";
-const network = "http://bops.morpheuslabs.io:23036";
+const network = "http://bops-t.morpheuslabs.io:28013";
 
 
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
